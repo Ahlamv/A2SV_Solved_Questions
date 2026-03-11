8bd275fdf551fd244fcc9cc6cbaf586f207fb445
Ahlam Ahmed <ahlamahmedali22@gmail.com>
2026-03-11 16:59:30 +0300

    Add solution for Crawler Log Folder

---
+++ leetcode/1598-crawler-log-folder/1598-crawler-log-folder.py
@@ -0,0 +1,13 @@
+class Solution:
+    def minOperations(self, logs: List[str]) -> int:
+        stack=[]
+        for dir in logs:
+            if dir == "../":
+                if stack:
+                    stack.pop()
+            elif dir == "./":
+                continue
+            else:
+                stack.append(dir)
+        return len(stack)
+             
