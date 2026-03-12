84b9c7857d1020c8a76229d610bb7f58c50226c5
Ahlam Ahmed <ahlamahmedali22@gmail.com>
2026-03-12 15:16:32 +0300

    Add solution for Array-Subset-Practice

---
+++ geeksforgeeks/Array-Subset-Practice/Array-Subset-Practice.py
@@ -0,0 +1,19 @@
+class Solution:
+
+    def isSubset(self, a, b):
+        a.sort()
+        b.sort()
+        i = j = 0
+        n, m = len(a), len(b)
+        
+        while i < n and j < m:
+            if a[i] < b[j]:
+                i += 1
+            elif a[i] == b[j]:
+                i += 1
+                j += 1
+            else: 
+                return False
+        
+
+        return j == m
