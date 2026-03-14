2b630f15c9b387671833ef6edecde6bf5c036279
Ahlam Ahmed <ahlamahmedali22@gmail.com>
2026-03-14 23:23:04 +0300

    Add solution for Single Number

---
+++ leetcode/0136-single-number/0136-single-number.py
@@ -0,0 +1,18 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        nums.sort()
+        n = len(nums)
+        i = 0
+        while i < n-1:
+            if nums[i] != nums[i+1]:
+                return nums[i]
+            i += 2
+        return nums[-1]
+
+                
+
+
+          
+
+             
+        
