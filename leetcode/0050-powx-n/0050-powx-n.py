6a055ebe27e88de0e1518bcb80e902dc5fdae344
Ahlam Ahmed <ahlamahmedali22@gmail.com>
2026-03-17 17:53:48 +0300

    Add solution for Pow(x, n)

--- leetcode/0050-powx-n/0050-powx-n.py
+++ leetcode/0050-powx-n/0050-powx-n.py
@@ -1,19 +1,17 @@
 class Solution:
     def myPow(self, x: float, n: int) -> float:
-    
-        def power(n):
-            if n==1:
-                return x
-            if n==0:
-                return 1
-            h=power(n//2)
-            if n%2==0:
-                return h*h
-            else:
-                return h*h*x
-        if n < 0:
-            return 1 / power(-n)
-        return power(n)
+        if n<0:
+            return 1/self.myPow(x,-n)
+        if n==0:
+            return 1
+        half=self.myPow(x,n//2)
+        if n%2==0:
+            return half*half
+        else:
+            return half*half*x
+        
+
+
 
 
 
