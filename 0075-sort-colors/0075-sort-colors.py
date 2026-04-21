b8f6d3c79f738045ad79125b5080c4822b187e0b
Ahlam Ahmed <ahlamahmedali22@gmail.com>
2026-04-21 17:42:09 +0300

    Time: 0 ms (100%), Space: 19.4 MB (6.85%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -7,8 +7,11 @@ class Solution:
             if nums[mid] == 2:
                 nums[mid], nums[right] = nums[right], nums[mid]
                 right-=1
-            if nums[mid] == 0:
+            elif nums[mid] == 0:
                 nums[mid], nums[left] = nums[left], nums[mid]
                 left+=1
-            mid+=1
+                mid+=1
+            else:
+                mid+=1
+           
         
